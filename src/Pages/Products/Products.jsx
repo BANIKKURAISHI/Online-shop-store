7cdeed372cd17d7e2e917a2a7fe3487490615ff4
BANIKKURAISHI <mdkuraishirahmanbanik@gmail.com>
2023-09-22 11:56:48 +0600

    This is awsome

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -53,15 +53,15 @@ const useHandleNavigate=()=>{
         </div>
         <div className="flex items-center justify-between">
             <span className="text-3xl font-bold text-gray-900 dark:text-white">{price}</span>
-           <Link to={`/products/${id}`}>
+            <Link to={`/products/${id}`}>
             <button  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View Details</button>
-        </Link> 
+        </Link>  
         <button onClick={useHandleNavigate} to={`/products/${id}`} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View Details</button>
         </div>
     </div>
 </div>
 
-        </div>
+</div>
     );
 };
 Products.propTypes={
